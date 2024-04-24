e2e510b309d0b0cd24b94d2fe69226bd85f4a55c
chatmindsolution <skfox@pm.me>
2024-04-24 14:33:12 -0700

    no badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -37,25 +37,25 @@ export const Badge = (props: Props) => {
   });
 
   return (
- /*   <span
+    <span
       class="w-full text-center px-[10px] pt-[6px] pb-[10px] m-auto text-[13px]"
       style={{
         color: props.poweredByTextColor ?? defaultTextColor,
-        'background-color': props.badgeBackgroundColor ?? '#ffffff',
+        'background-color': props.badgeBackgroundColor ?? '#303235',
       }}
     >
       Powered by
       <a
         ref={liteBadge}
-        href={'https://chatmindsolution.com'}
+        href={''}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> ChatMind</span>
+        <span> </span>
       </a>
-   </span> */
+    </span>
   );
 };
